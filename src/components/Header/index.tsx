d55da601d7298cdbe5c761478a6308843b186c8f
Abeba Ngwe <abi@mesaidespubliques.fr>
2022-06-06 00:46:29 +0200

    feat: used & styled Algolia SearchBox

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
+import { FC } from "react";
+import { SearchBox } from "react-instantsearch-hooks-web";
 import Logo from "@/ui/logo.svg?component";
 
 import "./Header.sass";
 
-function Header() {
-  return <Logo />;
-}
+const Header: FC = () => (
+  <header>
+    <Logo />
+    <SearchBox placeholder="Restaurant name, food type, city..." />
+  </header>
+);
 
 export default Header;
