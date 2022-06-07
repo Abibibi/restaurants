89770f804f2dc0ca549be16ffea96fc2c54113aa
Abeba Ngwe <abi@mesaidespubliques.fr>
2022-06-07 05:29:34 +0200

    refactor: merge resolved

--- src/components/Records/Record/index.tsx
+++ src/components/Records/Record/index.tsx
@@ -8,38 +8,45 @@ import { Record as RecordType } from "@/helpers/interfaces";
 
 import "./Record.sass";
 
-const Record: FC<RecordType> = ({ hit }: RecordType) => (
-  <div className="record">
-    <div className="record-picture">
-      <img src={hit.image_url} alt={hit.name} />
+const Record: FC<RecordType> = ({ hit }: RecordType) => {
+  function deleteRecord(): void {}
+
+  return (
+    <div className="record">
+      <div className="record-picture">
+        <img src={hit.image_url} alt={hit.name} />
+      </div>
+      <div className="record-details">
+        <div className="record-details-type-remove">
+          <span>{hit.food_type.toLocaleUpperCase()}</span>
+          <div
+            className="record-details-type-remove-bin"
+            onClick={deleteRecord}
+          >
+            <BsTrash />
+          </div>
+        </div>
+        <div className="record-details-name-stars">
+          <h2>{hit.name}</h2>
+          <div className="record-details-name-stars-mark">
+            <span>{hit.stars_count}</span>
+            <Rater total={5} rating={hit.stars_count} interactive={false} />
+          </div>
+        </div>
+        <div className="record-details-address-reviews">
+          <div className="record-details-address-reviews-localization">
+            {`${hit.address}, ${hit.city}, ${hit.state}, ${hit.postal_code}`}
+          </div>
+          <div className="record-details-address-reviews-count">
+            <ImBubble2 /> {hit.reviews_count}
+          </div>
+        </div>
+        <div className="record-details-price">
+          {Array.from(Array(hit.price)).map(() => "$")}
+        </div>
+      </div>
     </div>
-    <div className="record-details">
-      <div className="record-details-type-remove">
-        <span>{hit.food_type.toLocaleUpperCase()}</span>
-        <div className="record-details-type-remove-bin">
-          <BsTrash />
-        </div>
-      </div>
-      <div className="record-details-name-stars">
-        <h2>{hit.name}</h2>
-        <div className="record-details-name-stars-mark">
-          <span>{hit.stars_count}</span>
-          <Rater total={5} rating={hit.stars_count} interactive={false} />
-        </div>
-      </div>
-      <div className="record-details-address-reviews">
-        <div className="record-details-address-reviews-localization">
-          {`${hit.address}, ${hit.city}, ${hit.state}, ${hit.postal_code}`}
-        </div>
-        <div className="record-details-address-reviews-count">
-          <ImBubble2 /> {hit.reviews_count}
-        </div>
-      </div>
-      <div className="record-details-price">
-        {Array.from(Array(hit.price)).map(() => "$")}
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Record;
