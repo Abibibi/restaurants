52bf894bd165f9dc42ff088cf9cc02fd05836498
Abeba Ngwe <abi@mesaidespubliques.fr>
2022-06-07 14:04:40 +0200

    bug: use of CustomHits: list of records now immediately updated when one record is deleted

--- src/components/Records/index.tsx
+++ src/components/Records/index.tsx
@@ -1,19 +1,76 @@
-import { FC } from "react";
-import { Hits, Pagination, InfiniteHits } from "react-instantsearch-hooks-web";
+import { FC, useEffect, useState } from "react";
+import {
+  Hits,
+  Pagination,
+  useHits,
+  UseHitsProps,
+  useInfiniteHits,
+  UseInfiniteHitsProps,
+} from "react-instantsearch-hooks-web";
 
 import { Mobile, TabletOrDesktop } from "@/helpers/media";
 import Record from "@/components/Records/Record";
 
 import "./Records.sass";
+import { Hit } from "@/helpers/interfaces";
+
+const CustomInfiniteHits: FC<UseInfiniteHitsProps> = (
+  props: UseInfiniteHitsProps
+) => {
+  const { hits, showMore, isLastPage } = useInfiniteHits(props);
+  const [displayedHits, setDisplayedHits] = useState<Hit[]>([]);
+
+  function setHitsAfterDeletion(hitId: string): void {
+    setDisplayedHits(displayedHits.filter((hit) => hit.objectID !== hitId));
+  }
+
+  useEffect(() => {
+    setDisplayedHits(hits as any);
+  }, [hits]);
+
+  return (
+    <>
+      {displayedHits.map((hit) => (
+        <Record hit={hit} key={hit.objectID} resetHits={setHitsAfterDeletion} />
+      ))}
+      {!isLastPage && (
+        <button className="records-show-more" type="button" onClick={showMore}>
+          Show more
+        </button>
+      )}
+    </>
+  );
+};
+
+const CustomHits: FC<UseHitsProps> = (props: UseHitsProps) => {
+  const { hits } = useHits(props);
+  const [displayedHits, setDisplayedHits] = useState<Hit[]>([]);
+
+  function setHitsAfterDeletion(hitId: string): void {
+    setDisplayedHits(displayedHits.filter((hit) => hit.objectID !== hitId));
+  }
+
+  useEffect(() => {
+    setDisplayedHits(hits as any);
+  }, [hits]);
+
+  return (
+    <>
+      {displayedHits.map((hit) => (
+        <Record hit={hit} key={hit.objectID} resetHits={setHitsAfterDeletion} />
+      ))}
+    </>
+  );
+};
 
 const Records: FC = () => (
   <div className="records">
     <Mobile>
-      <InfiniteHits hitComponent={Record} showPrevious={false} />
+      <CustomInfiniteHits />
     </Mobile>
 
     <TabletOrDesktop>
-      <Hits hitComponent={Record} />
+      <CustomHits />
       <Pagination showFirst={false} showLast={false} />
     </TabletOrDesktop>
   </div>
